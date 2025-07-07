e69d76300062c71da5dba73152bbd1e9f0416184
doodlezucc <electerminator@gmail.com>
2025-07-07 21:13:25 +0200

    Remove generated files from repository

--- package/dist/index.js
+++
@@ -1,3 +0,0 @@
-export { syncroState } from './proxys/syncroState.svelte.js';
-export { usePresence } from './presence.svelte.js';
-export { y } from './schemas/schema.js';
--- package/dist/proxys/common.d.ts
+++
@@ -1,5 +0,0 @@
-import type { SyncedArray } from './array.svelte.js';
-import type { SyncedSet } from './set.svelte.js';
-import type { SyncedObject } from './object.svelte.js';
-import type { SyncedMap } from './map.svelte.js';
-export type SyncedContainer = SyncedObject | SyncedArray | SyncedSet | SyncedMap;
