57dd9f47ebd89021e16463cef4a100a53a9850fe
beynar <arnaud@derbey.dev>
2025-08-27 23:01:52 +0200

    first fixes

--- package/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/proxy/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.CyMVsvzx.js","_app/immutable/chunks/disclose-version.DsQ_o76s.js","_app/immutable/chunks/runtime.BVl7ZLM4.js","_app/immutable/chunks/render.DQICMmcn.js","_app/immutable/chunks/sources.BIb9Frml.js","_app/immutable/chunks/this.fPLeM_sq.js","_app/immutable/chunks/proxy.B3p0uDm0.js"];
-export const stylesheets = [];
-export const fonts = [];
